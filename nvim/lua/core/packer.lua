d1451013f06f6d22a47234bcc498f28e7dd22278
kevinchangjk <kevinchangjk@gmail.com>
2022-07-26 21:18:42 +0800

    nvim: reorganised packer files

--- nvim/lua/core/packer.lua
+++ nvim/lua/core/packer.lua
@@ -9,30 +9,45 @@ vim.cmd([[
   augroup end
 ]])
 
+package.path = package.path .. ";../plugins/?.lua"
+
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-fzy-native.nvim'
-  use 'BurntSushi/ripgrep'
+
+  -- telescope & dependencies
+  use {'nvim-telescope/telescope.nvim',
+    requires = {
+      'nvim-telescope/telescope-fzy-native.nvim', 
+      'nvim-lua/popup.nvim', 
+      'nvim-lua/plenary.nvim', 
+      'BurntSushi/ripgrep'
+    }
+  }
+
+  -- colorschemes
   use 'flazz/vim-colorschemes'
   use '$XDG_CONFIG_HOME/nvim/monokai.nvim'
+
+  -- useful utility
   use 'mbbill/undotree'
   use 'preservim/nerdtree'
   use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-lualine/lualine.nvim'
+  use 'ackyshake/VimCompletesMe'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'psliwka/vim-smoothie'
+  use {'prettier/vim-prettier', run = 'yarn install --frozen-lockfile --production' }  
+
+  -- lsp
+  use 'sheerun/vim-polyglot'
   use 'neovim/nvim-lspconfig'
   use 'leafgarland/typescript-vim'
   use 'tomlion/vim-solidity'
   use 'maxmellon/vim-jsx-pretty'
-  use {'prettier/vim-prettier', run = 'yarn install --frozen-lockfile --production' }  
-  use 'ThePrimeagen/vim-be-good'
-  use 'ackyshake/VimCompletesMe'
-  use 'nvim-lualine/lualine.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'psliwka/vim-smoothie'
-  use 'sheerun/vim-polyglot'
   use 'rust-lang/rust.vim'
+
+  -- misc stuff
+  use 'ThePrimeagen/vim-be-good'
   use 'wakatime/vim-wakatime'
 end)
