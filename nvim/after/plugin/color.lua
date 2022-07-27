7e4e33d2776f36783a5f1b7bc438061f44bfb5ba
kevinchangjk <kevinchangjk@gmail.com>
2022-07-28 00:17:07 +0800

    nvim: changed colors for lualine and command line

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,11 +1,18 @@
 -- colour scheme
-require('onedark').setup()
+require('onedark').setup({
+    colors = { fg0 = "#D6E2F0" },
+
+    -- Overwrites color
+    -- Notably, for the message area / command line
+    overrides = function(c) 
+      return {
+        MsgArea = { bg = c.bg1 },
+        ColorColumn = { bg = c.bg1 },
+      }
+    end,
+
+    -- Makes background transparent
+    transparent = true,
+  })
 
 vim.opt.colorcolumn = '80'
-
--- setting background
-vim.cmd([[
-  highlight ColorColumn guibg=#2E323C
-  highlight NonText guibg=none
-  highlight Normal guibg=none
-  ]])
--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,8 +1,5 @@
 require'lualine'.setup{
   options = {
     theme = 'onedark-nvim', 
-    icons_enabled = false,
-    component_separators = { left = '|', right = '|'},
-    section_separators = { left = '[', right = ']' }
     }
 }
