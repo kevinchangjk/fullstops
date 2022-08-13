5c207dd7fad724510f08bdb2a278e86b1cec0a34
kevinchangjk <kevinchangjk@gmail.com>
2022-08-13 10:08:21 +0800

    nvim: take out augroup for autoindent

--- nvim/after/plugin/augroup.lua
+++ nvim/after/plugin/augroup.lua
@@ -1,11 +1,11 @@
 local api = vim.api
 
 -- indent files properly upon write
-local readWriteGrp = api.nvim_create_augroup("ReadWrite", { clear = true })
-api.nvim_create_autocmd("BufWritePre", {
-    command = [[normal K=J``]],
-    group = readWriteGrp,
-  })
+-- local readWriteGrp = api.nvim_create_augroup("ReadWrite", { clear = true })
+-- api.nvim_create_autocmd("BufWritePre", {
+--     command = [[normal K=J``]],
+--     group = readWriteGrp,
+--   })
 
 -- go to last loc when opening a buffer
 api.nvim_create_autocmd("BufReadPost", {
