5d5d2444eb5a17b286194ef8536128f3288e8b6a
kevinchangjk <kevinchangjk@gmail.com>
2022-08-14 14:47:48 +0800

    nvim: made java indents 4 spaces for nus cs mods

--- nvim/after/plugin/augroup.lua
+++ nvim/after/plugin/augroup.lua
@@ -1,14 +1,42 @@
 local api = vim.api
 
+local readWriteGrp = api.nvim_create_augroup("ReadWrite", { clear = true })
+local nusModGrp = api.nvim_create_augroup("NusMod", { clear = true })
+local navGrp = api.nvim_create_augroup("NavGrp", { clear = true })
+
 -- indent files properly upon write
--- local readWriteGrp = api.nvim_create_augroup("ReadWrite", { clear = true })
 -- api.nvim_create_autocmd("BufWritePre", {
 --     command = [[normal K=J``]],
 --     group = readWriteGrp,
 --   })
 
 -- go to last loc when opening a buffer
-api.nvim_create_autocmd("BufReadPost", {
+api.nvim_create_autocmd(
+  "BufReadPost", {
     command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]],
     group = readWriteGrp,
+  })
+
+-- windows to close with "q"
+api.nvim_create_autocmd(
+  "FileType", {
+    pattern = { "help", "startuptime", "qf", "lspinfo" },
+    command = [[nnoremap <buffer><silent> q :close<CR>]],
+    group = navGrp,
+  })
+
+api.nvim_create_autocmd(
+  "FileType", {
+    pattern = "man",
+    command = [[nnoremap <buffer><silent> q :quit<CR>]],
+    group = navGrp,
+  })
+
+-- nus cs mod specific
+-- for java files, make indents 4 spaces
+api.nvim_create_autocmd(
+  "FileType", {
+    pattern = { "java" },
+    command = [[set tabstop=4 softtabstop=4 shiftwidth=4]],
+    group = nusModGrp,
   })
