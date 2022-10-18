c66062d3775b97b91a6f256cb72a5e7d9c21c827
kevinchangjk <kevinchangjk@gmail.com>
2022-10-18 18:37:53 +0800

    nvim: changed telescope border colors

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -8,7 +8,8 @@ require('onedark').setup({
       return {
         MsgArea = { bg = c.bg1 },
         ColorColumn = { bg = c.bg1 },
-        NormalFloat = { bg = nil },
+        NormalFloat = { bg = c.bg1 },
+        TelescopeBorder = { bg = c.bg_visual },
       }
     end,
 
