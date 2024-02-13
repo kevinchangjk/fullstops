d5733fe4c3d6f11056d40830c04d4e9a648cdb46
kevinchangjk <kevinchangjk@gmail.com>
2024-02-13 12:28:36 +0800

    nvim: add keymap for toggling copilot

---
+++ nvim/after/plugin/copilot.lua
@@ -0,0 +1,7 @@
+local nnoremap = Map.noremap("n")
+
+-- sets spacebar + e as toggle
+nnoremap("<leader>ce", ":Copilot enable<cr>")
+
+-- sets spacebar + d as toggle
+nnoremap("<leader>cd", ":Copilot disable<cr>")
